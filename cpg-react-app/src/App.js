12900e6f5b319979c7324f33552cad83486f5c9a
MorpheusOQug <qahp955@gmail.com>
2025-02-27 00:53:57 +0700

    Add .gitattributes to handle line endings

--- cpg-react-app/src/App.js
+++ cpg-react-app/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useEffect, useState } from 'react';
+import axios from './axiosConfig';  
+
+const App = () => {
+  const [message, setMessage] = useState('Connected to backend successfully');
+
+  useEffect(() => {
+    axios.get('/')  
+      .then(response => {
+        setMessage(response.data);  
+      })
+      .catch(error => {
+        console.error('Something wrongs about connect to backend:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{message}</h1>
     </div>
   );
-}
+};
 
 export default App;
+
