a37002f53fbb6940ef7534932f33fec1ea331dc7
MorpheusOQug <qahp955@gmail.com>
2025-03-07 13:28:32 +0700

    Revert "FE v1"
    This reverts commit d5badf83f86c1448f8fd40ab24f0f70c1da846d4.

--- cpg-react-app/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import footerImage from "../assets/footer.svg";
-
-export default function Footer() {
-  return (
-    <div className="mt-20">
-      <img src={footerImage} alt="Footer" className="w-full" />
-    </div>
-  );
-}
